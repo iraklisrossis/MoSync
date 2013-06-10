7efcb0cf60c3bbb719fe618f34505a613622f91a
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2013-06-10 13:16:00 +0200

    BB10 now supports example HelloNativeUI.

--- runtimes/cpp/platforms/bb10/MoSync/src/NativeUI.cpp
+++ runtimes/cpp/platforms/bb10/MoSync/src/NativeUI.cpp
@@ -7,15 +7,28 @@
 #include "event.h"
 #include "MemStream.h"
 #include "Syscall.h"
+#include "bbutil.h"
 
 #include <hash_map>
 #include <assert.h>
 #include <unistd.h>
 #include <limits.h>
 
+#include <bb/cascades/AbsoluteLayout>
 #include <bb/cascades/Application>
 #include <bb/cascades/Button>
+#include <bb/cascades/CheckBox>
+#include <bb/cascades/Container>
+#include <bb/cascades/ForeignWindowControl>
+#include <bb/cascades/ImageButton>
+#include <bb/cascades/ImageView>
+#include <bb/cascades/Label>
+#include <bb/cascades/ListView>
+#include <bb/cascades/NavigationPane>
 #include <bb/cascades/Page>
+#include <bb/cascades/StackLayout>
+#include <bb/cascades/StandardListItem>
+#include <bb/cascades/TextField>
 #include <bb/cascades/TitleBar>
 #include <bb/cascades/WebView>
 #include <bb/AbstractBpsEventHandler>
@@ -32,9 +45,21 @@ using namespace bb::cascades;
 using namespace std;
 
 enum WidgetType {
-	eScreen,
+	eNull,
+	ePage,
 	eWebView,
 	eButton,
+	eLayout,
+	eLabel,
+	eTextField,
+	eForeignWindowControl,
+	eImageButton,
+	eNavigationPane,
+	eListView,
+	eStandardListItem,
+	eAbsoluteLayout,
+	eCheckBox,
+	eImageView,
 };
 
 enum WidgetFunction {
@@ -42,6 +67,8 @@ enum WidgetFunction {
 	EVENT_CODE_WIDGET_ADD_CHILD,
 	EVENT_CODE_WIDGET_SET_PROPERTY,
 	EVENT_CODE_WIDGET_SCREEN_SHOW,
+	EVENT_CODE_STACK_SCREEN_PUSH,
+	EVENT_CODE_STACK_SCREEN_POP,
 };
 
 struct Widget {
@@ -80,6 +107,8 @@ static int nuiCreate(const bps_event_payload_t& payload);
 static int nuiAddChild(const bps_event_payload_t& payload);
 static int nuiSetProperty(const bps_event_payload_t& payload);
 static int nuiScreenShow(const bps_event_payload_t& payload);
+static int nuiStackScreenPush(const bps_event_payload_t& payload);
+static int nuiStackScreenPop(const bps_event_payload_t& payload);
 
 int nuiFunction(int code, const bps_event_payload_t& payload) {
 	switch(code) {
@@ -87,11 +116,12 @@ int nuiFunction(int code, const bps_event_payload_t& payload) {
 	case EVENT_CODE_WIDGET_ADD_CHILD: return nuiAddChild(payload);
 	case EVENT_CODE_WIDGET_SET_PROPERTY: return nuiSetProperty(payload);
 	case EVENT_CODE_WIDGET_SCREEN_SHOW: return nuiScreenShow(payload);
+	case EVENT_CODE_STACK_SCREEN_PUSH: return nuiStackScreenPush(payload);
+	case EVENT_CODE_STACK_SCREEN_POP: return nuiStackScreenPop(payload);
 	default:
 		DEBIG_PHAT_ERROR;
 	}
 }
-
 
 int runCascadeApp(int argc, char** argv, int (*func)(void*)) {
 	Application app(argc, argv);
@@ -112,26 +142,32 @@ static const Widget& getWidget(MAWidgetHandle handle) {
 	return itr->second;
 }
 
-MAWidgetHandle maWidgetCreate(const char* widgetType) {
-	LOG("maWidgetCreate(%s);\n", widgetType);
+// run on UI thread.
+static int run(WidgetFunction f, uintptr_t data1=0, uintptr_t data2=0, uintptr_t data3=0) {
 	bps_event_t* be;
-	bps_event_payload_t payload = { (uintptr_t)widgetType, 0, 0 };
-	BPSERR(bps_event_create(&be, sNuiEventDomain, EVENT_CODE_WIDGET_CREATE, &payload, NULL));
+	bps_event_payload_t payload = { data1, data2, data3 };
+	BPSERR(bps_event_create(&be, sNuiEventDomain, f, &payload, NULL));
 	BPSERR(bps_channel_push_event(sNuiEventChannel, be));
 	sNuiSem.wait();
 	return sNuiReturnValue;
 }
 
+MAWidgetHandle maWidgetCreate(const char* widgetType) {
+	LOG("maWidgetCreate(%s);\n", widgetType);
+	return run(EVENT_CODE_WIDGET_CREATE, (uintptr_t)widgetType);
+}
+
 static int nuiCreate(const bps_event_payload_t& payload) {
 	const char* widgetType = (char*)payload.data1;
 	UIObject* u = NULL;
-	WidgetType t;
+	WidgetType t = eNull;
 	Handler* h = NULL;
 	if(strcmp(widgetType, MAW_SCREEN) == 0) {
 		Page* page = Page::create();
 		u = page;
-		t = eScreen;
+		t = ePage;
 	}
+	else
 	if(strcmp(widgetType, MAW_WEB_VIEW) == 0) {
 		WebView* w = WebView::create();
 		h = new Handler(sNextWidgetHandle);
@@ -139,10 +175,108 @@ static int nuiCreate(const bps_event_payload_t& payload) {
 		t = eWebView;
 		assert(QObject::connect(w, SIGNAL(messageReceived(const QVariantMap&)), h, SLOT(messageReceived(const QVariantMap&))));
 	}
+	else
+	if(strcmp(widgetType, MAW_VERTICAL_LAYOUT) == 0) {
+		// Cheaper than create().
+		StackLayout* s = new StackLayout();
+		// This is the default, but let's set it anyway to be clear and sure.
+		s->setOrientation(LayoutOrientation::TopToBottom);
+		Container* c = new Container();
+		c->setLayout(s);
+		u = c;
+		t = eLayout;
+	}
+	else
+	if(strcmp(widgetType, MAW_HORIZONTAL_LAYOUT) == 0) {
+		StackLayout* s = new StackLayout();
+		s->setOrientation(LayoutOrientation::LeftToRight);
+		u = s;
+		t = eLayout;
+	}
+	else
+	if(strcmp(widgetType, MAW_LABEL) == 0) {
+		u = new Label();
+		t = eLabel;
+	}
+	else
+	if(strcmp(widgetType, MAW_EDIT_BOX) == 0) {
+		u = new TextField();
+		t = eTextField;
+	}
+	else
+	if(strcmp(widgetType, MAW_GL_VIEW) == 0) {
+		ForeignWindowControl* f = new ForeignWindowControl();
+#if 0	// doesn't work.
+		screen_window_t sw = f->windowHandle();
+#else
+		screen_context_t screen_ctx;
+    ERRNO(screen_create_context(&screen_ctx, SCREEN_APPLICATION_CONTEXT));
+
+		screen_window_t sw;
+		ERRNO(screen_create_window(&sw, screen_ctx));
+
+		QByteArray groupArr = f->windowGroup().toAscii();
+		ERRNO(screen_join_window_group(sw, groupArr.constData()));
+
+		int format = SCREEN_FORMAT_RGBA8888;
+		ERRNO(screen_set_window_property_iv(sw, SCREEN_PROPERTY_FORMAT, &format));
+#endif
+		bbutil_init_egl(sw, 0);
+
+		// I hear this is useful for performance.
+		int z = 1;
+		screen_set_window_property_iv(sw, SCREEN_PROPERTY_ZORDER, &z);
+
+		u = f;
+		t = eForeignWindowControl;
+	}
+	else
+	if(strcmp(widgetType, MAW_BUTTON) == 0) {
+		u = new Button();
+		t = eButton;
+	}
+	else
+	if(strcmp(widgetType, MAW_IMAGE) == 0) {
+		u = new ImageView();
+		t = eImageView;
+	}
+	else
+	if(strcmp(widgetType, MAW_IMAGE_BUTTON) == 0) {
+		u = new ImageButton();
+		t = eImageButton;
+	}
+	else
+	if(strcmp(widgetType, MAW_STACK_SCREEN) == 0) {
+		u = NavigationPane::create();
+		t = eNavigationPane;
+	}
+	else
+	if(strcmp(widgetType, MAW_LIST_VIEW) == 0) {
+		u = new ListView();
+		t = eListView;
+	}
+	else
+	if(strcmp(widgetType, MAW_LIST_VIEW_ITEM) == 0) {
+		u = new StandardListItem();
+		t = eStandardListItem;
+	}
+	else
+	if(strcmp(widgetType, MAW_RELATIVE_LAYOUT) == 0) {
+		Container* c = new Container();
+		c->setLayout(new AbsoluteLayout());
+		u = c;
+		t = eAbsoluteLayout;
+	}
+	else
+	if(strcmp(widgetType, MAW_CHECK_BOX) == 0) {
+		u = new CheckBox();
+		t = eCheckBox;
+	}
 
 	if(!u) {
 		DEBIG_PHAT_ERROR;
 	}
+	DEBUG_ASSERT(t != eNull);
 	LOG("Widget created: %i\n", sNextWidgetHandle);
 	sWidgets[sNextWidgetHandle] = { u, t, h };
 	return sNextWidgetHandle++;
@@ -193,7 +327,7 @@ static void postHookMessage(const Functor& arg) {
 	MAWidgetEventData *ed = new MAWidgetEventData;
 	ed->eventType = MAW_EVENT_WEB_VIEW_HOOK_INVOKED;
 	ed->widgetHandle = hmi.h;
-	ed->hookType = MAW_CONSTANT_MESSAGE;
+	ed->hookType = hmi.type;
 	ed->urlData = data;
 
 	MAEventNative event;
@@ -208,12 +342,7 @@ int maWidgetDestroy(MAWidgetHandle handle) {
 
 int maWidgetAddChild(MAWidgetHandle parentHandle, MAWidgetHandle childHandle) {
 	LOG("maWidgetAddChild(%i, %i);\n", parentHandle, childHandle);
-	bps_event_t* be;
-	bps_event_payload_t payload = { parentHandle, childHandle, 0 };
-	BPSERR(bps_event_create(&be, sNuiEventDomain, EVENT_CODE_WIDGET_ADD_CHILD, &payload, NULL));
-	BPSERR(bps_channel_push_event(sNuiEventChannel, be));
-	sNuiSem.wait();
-	return sNuiReturnValue;
+	return run(EVENT_CODE_WIDGET_ADD_CHILD, parentHandle, childHandle);
 }
 
 static int nuiAddChild(const bps_event_payload_t& payload) {
@@ -221,13 +350,24 @@ static int nuiAddChild(const bps_event_payload_t& payload) {
 	MAWidgetHandle childHandle = payload.data2;
 	const Widget& p = getWidget(parentHandle);
 	const Widget& c = getWidget(childHandle);
-	if(p.type == eScreen) {
-		Page* page = (Page*)p.o;
-		page->setContent((Control*)c.o);	//HACK
-	} else {
-		return MAW_RES_INVALID_LAYOUT;
+	switch(p.type) {
+	case ePage:
+		{
+			Page* page = (Page*)p.o;
+			page->setContent((Control*)c.o);	//HACK
+			return MAW_RES_OK;
+		}
+	case eLayout:
+	case eAbsoluteLayout:
+		{
+			Container* con = (Container*)p.o;
+			con->add((Control*)c.o);
+			return MAW_RES_OK;
+		}
+	default:
+		DEBIG_PHAT_ERROR;
+		//return MAW_RES_INVALID_LAYOUT;
 	}
-	return MAW_RES_OK;
 }
 
 int maWidgetInsertChild(MAWidgetHandle parentHandle, MAWidgetHandle childHandle, int index) {
@@ -249,19 +389,19 @@ int maWidgetModalDialogHide(MAWidgetHandle dialogHandle) {
 
 int maWidgetScreenShow(MAWidgetHandle screenHandle) {
 	LOG("maWidgetScreenShow(%i);\n", screenHandle);
-	bps_event_t* be;
-	bps_event_payload_t payload = { screenHandle, 0, 0 };
-	BPSERR(bps_event_create(&be, sNuiEventDomain, EVENT_CODE_WIDGET_SCREEN_SHOW, &payload, NULL));
-	BPSERR(bps_channel_push_event(sNuiEventChannel, be));
-	sNuiSem.wait();
-	return sNuiReturnValue;
+	return run(EVENT_CODE_WIDGET_SCREEN_SHOW, screenHandle);
 }
 
 static int nuiScreenShow(const bps_event_payload_t& payload) {
 	MAWidgetHandle screenHandle = payload.data1;
 	const Widget& s = getWidget(screenHandle);
-	DEBUG_ASSERT(s.type == eScreen);
-	APP.setScene((Page*)s.o);
+	AbstractPane* p;
+	switch(s.type) {
+	case ePage: p = (Page*)s.o; break;
+	case eNavigationPane: p = (NavigationPane*)s.o; break;
+	default: DEBIG_PHAT_ERROR;
+	}
+	APP.setScene(p);
 	return MAW_RES_OK;
 }
 
@@ -272,21 +412,34 @@ int maWidgetScreenShowWithTransition(MAWidgetHandle screenHandle,
 }
 
 int maWidgetStackScreenPush(MAWidgetHandle stackScreenHandle, MAWidgetHandle screenHandle) {
-	DEBIG_PHAT_ERROR;
+	LOG("maWidgetStackScreenPush(%i, %i);\n", stackScreenHandle, screenHandle);
+	return run(EVENT_CODE_STACK_SCREEN_PUSH, stackScreenHandle, screenHandle);
+}
+
+static int nuiStackScreenPush(const bps_event_payload_t& payload) {
+	const Widget& ss = getWidget(payload.data1);
+	const Widget& s = getWidget(payload.data2);
+	DEBUG_ASSERT(ss.type == eNavigationPane);
+	DEBUG_ASSERT(s.type == ePage);
+	((NavigationPane*)ss.o)->push((Page*)s.o);
+	return MAW_RES_OK;
 }
 
 int maWidgetStackScreenPop(MAWidgetHandle stackScreenHandle) {
-	DEBIG_PHAT_ERROR;
+	LOG("maWidgetStackScreenPop(%i);\n", stackScreenHandle);
+	return run(EVENT_CODE_STACK_SCREEN_POP, stackScreenHandle);
+}
+
+static int nuiStackScreenPop(const bps_event_payload_t& payload) {
+	const Widget& ss = getWidget(payload.data1);
+	DEBUG_ASSERT(ss.type == eNavigationPane);
+	((NavigationPane*)ss.o)->pop();
+	return MAW_RES_OK;
 }
 
 int maWidgetSetProperty(MAWidgetHandle handle, const char* key, const char* value) {
 	LOG("maWidgetSetProperty(%i, %s, %s);\n", handle, key, value);
-	bps_event_t* be;
-	bps_event_payload_t payload = { handle, (uintptr_t)key, (uintptr_t)value };
-	BPSERR(bps_event_create(&be, sNuiEventDomain, EVENT_CODE_WIDGET_SET_PROPERTY, &payload, NULL));
-	BPSERR(bps_channel_push_event(sNuiEventChannel, be));
-	sNuiSem.wait();
-	return sNuiReturnValue;
+	return run(EVENT_CODE_WIDGET_SET_PROPERTY, handle, (uintptr_t)key, (uintptr_t)value);
 }
 
 #if 0
@@ -328,6 +481,36 @@ static int nuiSetProperty(const bps_event_payload_t& payload) {
 			}
 		}
 		break;
+	case eLabel:
+		{
+			Label* l = (Label*)w.o;
+			if(strcmp(key, MAW_LABEL_TEXT) == 0) {
+				l->setText(value);
+				return MAW_RES_OK;
+			} else if(strcmp(key, MAW_LABEL_FONT_COLOR) == 0) {
+				// convert 6-digit hex color to uint.
+				DEBUG_ASSERT((strlen(value) == 6));
+				uint argb;
+				char* end;
+				argb = strtoul(value, &end, 16);
+				DEBUG_ASSERT(end - value == 6);
+
+				// non-transparent.
+				argb |= 0xff000000;
+
+				l->textStyle()->setColor(Color::fromARGB(argb));
+				return MAW_RES_OK;
+			}
+		}
+		break;
+	case eButton:
+		{
+			Button* b = (Button*)w.o;
+			if(strcmp(key, MAW_LABEL_TEXT) == 0) {
+				b->setText(value);
+				return MAW_RES_OK;
+			}
+		}
 	default:
 		break;
 	}
